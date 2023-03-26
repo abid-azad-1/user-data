44986042a0dd8d42ebc273e4d13210be07e7ee79
abid-azad-1 <abidazad143@gmail.com>
2023-03-26 16:30:50 +0600

    user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataLoad></DataLoad>
     </div>
   );
+}
+
+function DataLoad() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setData(data))
+  }, [])
+return(
+  <div>
+    <h1 className='data'><span className='-data'>Data:</span> {data.length}</h1>
+    <div className='user-data'>
+      {
+        data.map(userData => <AllData id={userData.id} name={userData.name} email={userData.email} address={userData.address.city} phone={userData.phone} website={userData.website}></AllData>)
+      }
+    </div>
+  </div>
+)
+
+}
+
+
+function AllData(props) {
+  return (
+    <div className='all-data'>
+      <p>User id: {props.id}</p>
+      <h3> <span className='name'>Name:</span> {props.name}</h3>
+      <h3> <span className='name'>Email:</span> {props.email}</h3>
+      <h3> <span className='name'>Address:</span> {props.address}</h3>
+      <h3> <span className='name'>Phone: </span> {props.phone}</h3>
+      <h3> <span className='name'>Website: </span> {props.website}</h3>
+      
+    </div>
+  )
 }
 
 export default App;
